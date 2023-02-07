969b61c4cac5cb7550b66be1598338499e0d3895
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-07 11:57:51 +0600

    about section added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTitle from '../../MyHooks/useTitle';
+import blogBg from '../../Assets/Banners/myPortfolioBlog.png';
+import { FaCheckCircle } from 'react-icons/fa';
 
 const Blog = () => {
+    const [more, setMore] = useState(false);
     useTitle('Blog');
+    const blogs = [
+        {
+            details: "I am much comfortable of UI designing. Click here to see the result."
+        },
+        {
+            details: "I am a full stack developer. I work with MERN Stack. Here is my full stack project."
+        },
+        {
+            details: "Web application experiences are shown here. Review my experiences."
+        }
+    ]
     return (
-        <div className='w-4/5 mx-auto pb-32'>
-            <h2 className='text-5xl text-emerald-500 font-bold'>Coming Soon...</h2>
-        </div>
+        <article className='w-4/5 mx-auto pb-32'>
+            <h2 className='text-5xl text-emerald-500 font-bold lf'>My Blog</h2>
+            <div className='grid lg:grid-cols-2 gap-5 my-10 items-center border-2 border-emerald-500 shadow-lg shadow-emerald-300 rounded-lg p-6'>
+                <div><img src={blogBg} alt="Banner-Background" className='rounded-md' /></div>
+                <div className='lg:ml-10'>
+                    <p>
+                        {
+                            blogs.map(blog => <>
+                                <div className='flex items-start my-4'>
+                                    <FaCheckCircle className='text-emerald-500 mr-2 text-xl' />
+                                    {
+                                        more ?
+                                            <p>{blog?.details}
+                                                {
+                                                    blog?.details.length > 70 &&
+                                                    <>
+                                                        ..<button onClick={() => setMore(!more)} className='text-left font-bold link-hover link-info'>read less</button>
+                                                    </>
+                                                }
+                                            </p>
+                                            :
+                                            <p>
+                                                {
+                                                    blog?.details.length > 70 ?
+                                                        <>
+                                                            {blog?.details.slice(0, 70)} ...<button
+                                                                onClick={() => setMore(!more)}
+                                                                className='font-bold link-hover link-info'>read more</button>
+                                                        </>
+                                                        :
+                                                        <>{blog?.details}</>
+                                                }
+                                            </p>
+                                    }
+                                </div>
+                            </>)
+                        }
+                    </p>
+                </div>
+            </div>
+        </article>
     );
 };
 
--- src/Pages/Projects/ProjectsCard.js
+++ src/Pages/Projects/ProjectsCard.js
@@ -9,19 +9,19 @@ const ProjectsCard = ({ project }) => {
         <div className="rounded-md lg:relative border-2 border-emerald-500 duration-500 text-white group-hover:blur-sm hover:!blur-none group-hover:scale-90 hover:!scale-100" style={{ boxShadow: '2px 3px 12px rgb(16, 185, 129)' }}>
             <PhotoProvider>
                 <PhotoView src={img}>
-                    <img src={img} alt="projectImg" className="w-full rounded-t-lg h-72 p-6 border-2 border-b-emerald-500 border-t-0 border-x-0 group-hover:blur-sm" />
+                    <img src={img} alt="projectImg" className="w-full rounded-t-lg h-72 p-6 border-2 border-b-emerald-500 border-t-0 border-x-0 lg:group-hover:blur-sm" />
                 </PhotoView>
             </PhotoProvider>
             <div className="lg:absolute lg:top-0 lg:left-[22%] lg:scale-0 group-hover:scale-100 duration-500 flex flex-col justify-between p-6 space-y-8">
                 <div className="space-y-2">
-                    <h2 className="text-3xl lg:text-emerald-500 font-semibold tracking-wide">{title}</h2>
+                    <h2 className="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-emerald-200 font-bold tracking-wide">{title}</h2>
                 </div>
                 <div className='flex justify-between items-center'>
-                    <a href={client} target='_blank'><p type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 hover:bg-emerald-500 text-emerald-500 hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}><FaGithub className='text-3xl' /></p></a>
-                    <a href={live} target="_blank"><p type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 hover:bg-emerald-500 text-emerald-500 hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}><FaGlobe className='text-3xl hover:animate-spin hover:duration-75' />
+                    <a href={client} target='_blank' className='bg-gray-200 rounded-md shadow-lg'><p type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 hover:bg-emerald-500 text-emerald-500 hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}><FaGithub className='text-3xl' /></p></a>
+                    <a href={live} target="_blank" className='bg-gray-200 rounded-md shadow-lg'><p type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 hover:bg-emerald-500 text-emerald-500 hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}><FaGlobe className='text-3xl hover:animate-spin hover:duration-75' />
                     </p></a>
                 </div>
-                <Link to={`/project/${_id}`} type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 hover:bg-emerald-500 text-emerald-500 uppercase hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}>details
+                <Link to={`/project/${_id}`} type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md border border-emerald-500 lg:bg-gray-200 hover:bg-emerald-500 text-emerald-500 uppercase hover:text-white" style={{ boxShadow: '2px 3px 5px gray' }}>details
                     <FaRegStickyNote className='ml-2' />
                 </Link>
             </div>
